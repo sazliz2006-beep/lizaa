59d16dab7b891efda4729957c8f0b13d39a4f604
sazliz2006-beep <sazliz2006@gmail.com>
2026-04-27 13:09:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ window.onload = function() {
     let preloader = document.getElementById('preloader');
     preloader.classList.add('hide-preloader');
 }
+document.querySelector('form')?.addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Форма отправлена (демонстрация). Данные: ' + new FormData(this));
+});
